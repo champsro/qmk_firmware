c46e8fa9c0c528c88fbb21890706f4684c09de6a
Rodrigo Pereira <champs.ro@gmail.com>
2020-01-23 18:18:09 -0300

    alterações compilação

--- keyboards/lambda/config.h
+++ keyboards/lambda/config.h
@@ -14,14 +14,9 @@
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 12
 
-#define MATRIX_ROW_PINS                \
-    {                                  \
-        F7, F6, F5, F4                 \
-    }
-#define MATRIX_COL_PINS                                \
-    {                                                  \
-        D3, D2, D1, D0, D4, C6, D7, E6, B4, B5, B6, B2 \
-    }
+#define MATRIX_ROW_PINS {F7, F6, F5, F4}
+#define MATRIX_COL_PINS {D3, D2, D1, D0, D4, C6, D7, E6, B4, B5, B6, B2}
+
 #define UNUSED_PINS
 
 #define DIODE_DIRECTION COL2ROW
